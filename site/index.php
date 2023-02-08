b4d558d236c67afece8c74b4405c6b8b3f9d34e5
Etienne van Bergeijk <etiennevanbergeijk@gmail.com>
2023-02-08 14:58:45 +0100

    ASSOCIATIEVE ARRAY

--- site/index.php
+++ site/index.php
@@ -1,10 +1,11 @@
 <?php 
 
-$abcGetallen = [["a","b","c","d"], [24,56,78,90]];
+$mijnGegevens = [
+    "voornaam" => "etienne ",
+    "achternaam" => "van bergeijk ",
+    "leeftijd" => 17
+];
 
-echo $abcGetallen [0][0]; 
-echo $abcGetallen [1][1];
-
-echo $abcGetallen [0][2];
-echo $abcGetallen [1][3];
-
+echo $mijnGegevens["voornaam"];
+echo $mijnGegevens["achternaam"];
+echo $mijnGegevens["leeftijd"];
