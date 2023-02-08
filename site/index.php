2e343c3df11be0bbe663560acda9fb475d41a59c
Etienne van Bergeijk <etiennevanbergeijk@gmail.com>
2023-02-08 09:51:28 +0100

    eenWillekeurigLijstje

--- site/index.php
+++ site/index.php
@@ -4,3 +4,14 @@ $eenAantalGetallen = [12,66,87,3000];
 echo $eenAantalGetallen[2];
 echo $eenAantalGetallen[2];
 
+
+$eenWillekeurigLijstje = [
+
+    0 => "etienne",
+    1 => "van Bergeijk",
+    2 => 17
+
+];
+
+
+
