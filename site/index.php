1d64e3a2cf8cfae3311e210ff714402536dddd73
Etienne van Bergeijk <etiennevanbergeijk@gmail.com>
2023-02-08 09:09:07 +0100

    opdracht Condities

--- site/index.php
+++ site/index.php
@@ -1,1 +1,8 @@
-<? echo "i am a big fan of the simpsons";
+<?php 
+
+$geboorteJaar = 2008;
+
+if($geboorteJaar > 1997 && $geboorteJaar < 2012){
+    echo "jij behoort tot de generatie z (zoomer) ";
+}
+
