45d4ae51758733c9834fd1d60ccc0d1d36d88528
Etienne van Bergeijk <etiennevanbergeijk@gmail.com>
2023-02-15 09:04:50 +0100

    show en select

---
+++ site/select.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="show.php">Show zonder waarde</a>
+            </li>
+            <li>
+                <a href="show.php?waarde1=honderd">Show met waarde 1</a>
+            </li>
+            <li>
+                <a href="show.php?waarde1=TweeHonderd&waarde2=drie">Show met 2 waardes</a>
+            </li>
+            <li>
+                <a href="show.php?waarde1=snoop%20Dog7waarde2=Dr%20Dre">snoop dog en Dr dre</a>
+            </li>
+        </ul>
+    </nav>
+</body>
+
+</html>
